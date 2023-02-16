3addf261a04da8ea12dfca87b2c4dfd62a945705
a8vk <a8vk@ya.ru>
2023-02-16 22:01:19 +1000

    add class Ship

--- sea_battle.py
+++ sea_battle.py
@@ -28,3 +28,31 @@ class BoardUsedException(BoardException):
 
 class BoardWrongShipException(BoardException):
     pass
+
+
+# Класс корабля
+class Ship:
+    def __init__(self, bow, parl, orient):  # параметр orient ориентация корабля (0 вертикальный, 1 горизонтальный)
+        self.bow = bow
+        self.parl = parl
+        self.orient = orient
+        self.lives = parl
+
+    @property
+    def dots(self):
+        ship_dots = []
+        for i in range(self.parl):
+            cur_x = self.bow.x
+            cur_y = self.bow.y
+
+            if self.orient == 0:
+                cur_x += i
+
+            elif self.orient == 1:
+                cur_y += i
+
+            ship_dots.append(Dot(cur_x, cur_y))
+        return ship_dots
+
+    def shoten(self, shot):  # метод показывает попадание в корабль
+        return shot in self.dots
