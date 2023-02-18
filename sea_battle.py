c3daf0b78d62cae662288eb718c589b64a07d628
a8vk <a8vk@ya.ru>
2023-02-18 17:13:27 +1000

    add class Color, Game, error correction

--- sea_battle.py
+++ sea_battle.py
@@ -107,13 +107,16 @@ class Board:
     def shot(self, d):  # стрельба по доске
         if self.out(d):  # выбрасываю исключение, точка за границей
             raise BoardOutException()
+
         if d in self.busy:  # выбрасываю исключение, точка занята
             raise BoardUsedException()
+
         self.busy.append(d)
+
         for ship in self.ships:
-            if ship.shooten(d):
+            if d in ship.dots:
                 ship.lives -= 1
-                self.field[d.x][d.y] = "x"  # корабль подбит
+                self.field[d.x][d.y] = "X"  # корабль подбит
                 if ship.lives == 0:
                     self.count += 1  # счётчик уничтоженных кораблей
                     self.contour(ship, verb=True)
@@ -122,8 +125,9 @@ class Board:
                 else:
                     print("Корабль подбит")
                     return True
+
         self.field[d.x][d.y] = "."
-        print("Мимо")
+        print("Мимо!")
         return False
 
     def begin(self):
@@ -132,7 +136,7 @@ class Board:
 
 # Класс игрока
 class Player:
-    def __int__(self, board, enemy):
+    def __init__(self, board, enemy):
         self.board = board
         self.enemy = enemy
 
@@ -141,7 +145,7 @@ class Player:
 
     def move(self):
         while True:
-            try:  # пытаемся выполнить выстрел
+            try:
                 target = self.ask()
                 repeat = self.enemy.shot(target)
                 return repeat
@@ -163,11 +167,104 @@ class User(Player):
         while True:
             cords = input("Ваш ход: ").split()
             if len(cords) != 2:
-                print("Введите две координаты")
+                print("Введите две координаты (координаты вводятся через пробел")
                 continue
             x, y = cords
-            if not(x.isdigit()) or not (y.isdigity()):
+            if not (x.isdigit()) or not (y.isdigit()):
                 print("Введите числа")
                 continue
             x, y = int(x), int(y)
             return Dot(x-1, y-1)
+
+
+# Класс цветной вывод (31 - красный, 32 - зелёный, 33 - жёлтый, 34 - синий)
+# https://habr.com/ru/sandbox/158854/
+class Color:
+    def out_color(self, color):
+        print(f"\033[{color}m{self}\033[0;0m")
+
+
+# noinspection PyTypeChecker
+class Game:
+    def __init__(self, size=6):
+        self.size = size
+        pl = self.random_board()
+        co = self.random_board()
+        co.hid = True
+
+        self.ai = AI(co, pl)
+        self.us = User(pl, co)
+
+    def random_board(self):
+        board = None
+        while board is None:
+            board = self.random_place()
+        return board
+
+    def random_place(self):
+        lens = [3, 2, 2, 1, 1, 1, 1]
+        board = Board(size=self.size)
+        attempts = 0
+        for ln in lens:
+            while True:
+                attempts += 1
+                if attempts > 2000:
+                    return None
+                ship = Ship(Dot(randint(0, self.size), randint(0, self.size)), ln, randint(0, 1))
+                try:
+                    board.add_ship(ship)
+                    break
+                except BoardWrongShipException:
+                    pass
+        board.begin()
+        return board
+
+    @staticmethod
+    def greet():
+        Color.out_color("*" * 20, 33)
+        Color.out_color(" " * 8 + "Game", 32)
+        Color.out_color(" " * 3 + "'Sea Battle'", 32)
+        print("")
+        Color.out_color(" " * 2 + "формат ввода: X Y", 34)
+        Color.out_color(" " * 2 + "X - номер строки", 34)
+        Color.out_color(" " * 2 + "Y - номер столбца", 34)
+        Color.out_color("*" * 20, 33)
+
+    def loop(self):
+        num = 0
+        while True:
+            print("-" * 20)
+            print("Доска пользователя:")
+            print(self.us.board)
+            print("-" * 20)
+            print("Доска компьютера:")
+            print(self.ai.board)
+            if num % 2 == 0:
+                print("-" * 20)
+                print("Ходит пользователь!")
+                repeat = self.us.move()
+            else:
+                print("-" * 20)
+                print("Ходит компьютер!")
+                repeat = self.ai.move()
+            if repeat:
+                num -= 1
+
+            if self.ai.board.count == 7:
+                print("-" * 20)
+                print("Пользователь выиграл!")
+                break
+
+            if self.us.board.count == 7:
+                print("-" * 20)
+                print("Компьютер выиграл!")
+                break
+            num += 1
+
+    def start(self):
+        self.greet()
+        self.loop()
+
+
+g = Game()
+g.start()
